11ae53e86b465f04bbcb74d3e49e174341705c1e
Kiman <59006202+KimanHeng@users.noreply.github.com>
2021-09-28 07:40:48 +0700

    Format & Optimize TipCalc3.py

--- Tip Calculator/TipCalc3.py
+++ Tip Calculator/TipCalc3.py
@@ -5,19 +5,19 @@ num_dish = int(input("Number of Dishes: "))
 
 dishes = []
 price = []
-for en in range(1, num_dish+1):
-    en_number = str(en)
+for en in range(num_dish):
+    en_number = str(en+1)
     dishes.append(input("Dish " + en_number + " Name: "))
     price.append(float(input("Dish " + en_number + " Price ($): ")))
 print("======================Happy Restaurant Bill======================")
-for res in range(1, num_dish+1):
-    res_num = str(res)
-    pc = str(price[res-1])
-    tx = str(format(float(price[res-1]*tax/100), '.2f'))
-    sr = str(format(float(price[res-1]*service/100), '.2f'))
-    print("Cost For Dish " + res_num + " | " + dishes[res-1] + " :", pc + ", Tax: " + tx + ", Service: " + sr)
-print("Subtotal Per Person: ", (sum(price) + (sum(price)*(tax+service)/100))/num_pep)
-print("Total Cost: ", sum(price) + (sum(price)*(tax+service)/100))
+for res in range(num_dish):
+    res_num = str(res+1)
+    pc = str(price[res])
+    tx = str(format(float(price[res]*tax/100), '.2f'))
+    sr = str(format(float(price[res]*service/100), '.2f'))
+    print("Cost For Dish " + res_num + " | " + dishes[res] + " :", pc + ", Tax: " + tx + ", Service: " + sr)
+print("Subtotal Per Person: ", format((sum(price) + (sum(price)*(tax+service)/100))/num_pep, '.2f'))
+print("Total Cost: ", format(sum(price) + (sum(price)*(tax+service)/100),'.2f'))
 print("==================================================================")
 print("Thank you for visiting \"Happy Restaurant\"")
 print("Come again soon!")
