83256892133f32f71628f3c0e839e31a1d3bee0b
Kiman <59006202+KimanHeng@users.noreply.github.com>
2021-09-29 14:14:08 +0700

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -4,13 +4,11 @@ def cipher_encrypt(message, key):
     for c in message:
         if c.isupper():
             c_index = ord(c) - ord('A')
-            c_shifted = (c_index + key) % 26 + ord('A')
-            c_new = chr(c_shifted)
+            c_new = chr((c_index + key) % 26 + ord('A'))
             encrypted += c_new
         elif c.islower():
             c_index = ord(c) - ord('a')
-            c_shifted = (c_index + key) % 26 + ord('a')
-            c_new = chr(c_shifted)
+            c_new = chr((c_index + key) % 26 + ord('a'))
             encrypted += c_new
         elif c.isdigit():
             c_new = (int(c) + key) % 10
@@ -26,31 +24,24 @@ def cipher_decrypt(message, key):
     for c in message:
         if c.isupper():
             c_index = ord(c) - ord('A')
-            c_og_pos = (c_index - key) % 26 + ord('A')
-            c_og = chr(c_og_pos)
-            decrypted += c_og
+            c_new = chr((c_index - key) % 26 + ord('A'))
+            decrypted += c_new
         elif c.islower():
             c_index = ord(c) - ord('a')
-            c_og_pos = (c_index - key) % 26 + ord('a')
-            c_og = chr(c_og_pos)
-            decrypted += c_og
+            c_new = chr((c_index - key) % 26 + ord('a'))
+            decrypted += c_new
         elif c.isdigit():
-            c_og = (int(c) - key) % 10
-            decrypted += str(c_og)
+            c_new = (int(c) - key) % 10
+            decrypted += str(c_new)
         else:
             decrypted += c
     return decrypted
 
 
-choice = int(input("This is Caesar Cipher\n1. Encrypt Message\n2. Decrypt Message\nYour Choice: "))
+choice = int(input("Caesir Cipher\n1. Encrypt Message\n2. Decrypt Message\nYour Choice: "))
 if choice == 1:
-    text = input("Plain Text: ")
-    result = cipher_encrypt(text, 5)
-    print("The plain text:\n", text)
-    print("Encrypted message:\n", result)
+    text = input("\nPlain Text: ")
+    print("Encrypted message:",cipher_encrypt(text, 5))
 elif choice == 2:
-    text = input("Cipher Text: ")
-    result = cipher_decrypt(text, 5)
-    print("The cipher text:\n", text)
-    print("Decrypted message:\n", result)
-
+    text = input("\nCipher Text: ")
+    print("Decrypted message:", cipher_decrypt(text, 5))
