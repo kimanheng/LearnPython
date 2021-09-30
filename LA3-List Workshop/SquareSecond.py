83418f7a81dfe2a9820c54aea0c94ed3d55a96a0
Kiman <59006202+KimanHeng@users.noreply.github.com>
2021-09-30 07:52:54 +0700

    Delete LA3-List Workshop directory

--- LA3-List Workshop/SquareSecond.py
+++
@@ -1,6 +0,0 @@
-numbers = [1, 2, 3, 4, 5]
-
-index = 1
-while index < len(numbers):
-	print(numbers[index] ** 2)
-	index = index + 2
--- LA3-List Workshop/SumThird.py
+++
@@ -1,9 +0,0 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-total = 0
-index = 2
-
-while index < len(numbers):
-    total = total + numbers[index]
-    index = index + 3
-
-print(total)
